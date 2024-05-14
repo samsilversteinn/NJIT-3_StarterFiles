28eff0446e14e668bcab19573c0dd3bb46eda60c
samsilversteinn <sesilversteinn@gmail.com>
2024-05-14 14:49:57 -0400

    step 7 part 2

--- site.js
+++ site.js
@@ -48,7 +48,20 @@ const vue_app = Vue.createApp({
               day = dateArray[2];
               month = months[monthIndex];
               return `${month} ${day}, ${year}`;
-        }
+        },
+        posterClick(index) {
+            if (this.movies[index].posterindex > this.movies[index].posters.length - 2) {
+                  this.movies[index].posterindex = 0
+            } else {
+                  this.movies[index].posterindex++
+            }
+
+      },
+      timeText(minutes) {
+            let hours = Math.trunc(minutes / 60);
+            let extraMinutes = minutes % 60;
+            return `${hours}h ${extraMinutes}m`;
+      }
   }
 })
 
